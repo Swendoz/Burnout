b3967cecb8fc6e98694c802e3c8044a43d2a190c
Alperen <alperenkenan40@gmail.com>
2024-04-02 10:26:06 +0200

    Burnout.sql updated

--- burnout.sql
+++ burnout.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 12 mrt 2024 om 11:00
+-- Gegenereerd op: 02 apr 2024 om 08:25
 -- Serverversie: 8.0.31
 -- PHP-versie: 7.4.33
 
@@ -26,6 +26,28 @@ USE `burnout`;
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'hypernano', '123'),
+(2, 'admin', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `posts`
 --
 
@@ -36,17 +58,17 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `message` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `posts`
 --
 
 INSERT INTO `posts` (`id`, `name`, `message`, `date`) VALUES
-(5, 'Hyper Nano', 'This is my message', '2024-03-12 10:33:24'),
-(6, 'Hyper Nano', 'Een burn-out ontstaat door langdurige stress en overbelasting, waarbij zowel fysieke als mentale energiereserves uitgeput raken.', '2024-03-12 10:57:07'),
-(7, 'Patetes KIzartmasi', 'OMG man this is perfect yuuuuu', '2024-03-12 10:57:17'),
-(8, 'Hyper Nano', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, cum suscipit modi quasi fugit ipsam quidem eligendi odio magnam culpa itaque quisquam illo perspiciatis obcaecati voluptatem odit rem architecto? Dignissimos!', '2024-03-12 10:57:58');
+(7, 'Patetes KIzartmasi', 'OMG man this is perfect yuuuuu', '2024-03-12 09:57:17'),
+(8, 'David Tenz', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, cum suscipit modi quasi fugit ipsam quidem eligendi odio magnam culpa itaque quisquam illo perspiciatis obcaecati voluptatem odit rem architecto? Dignissimos!', '2024-03-06 10:58:33'),
+(6, 'Emile', 'Een burn-out ontstaat door langdurige stress en overbelasting, waarbij zowel fysieke als mentale energiereserves uitgeput raken.', '2024-03-06 16:25:33'),
+(5, 'Hyper Nano', 'This is my message', '2024-03-06 09:25:33');
 
 -- --------------------------------------------------------
 
@@ -61,16 +83,19 @@ CREATE TABLE IF NOT EXISTS `survey` (
   `mail` varchar(255) NOT NULL,
   `score` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `survey`
 --
 
 INSERT INTO `survey` (`id`, `name`, `mail`, `score`) VALUES
-(1, 'Alperen Gedik', 'hhypernanoo@gmail.com', 100),
-(2, 'Hyper Nano', 'hhypernanoo@gmail.com', 67),
-(3, 'Hyper Nano', 'hhypernanoo@gmail.com', 67);
+(1, 'Lucas Kortleve', 'lucas@gmail.com', 100),
+(2, 'Robin', 'robin@gmail.com', 67),
+(3, 'Hyper Nano', 'hhypernanoo@gmail.com', 67),
+(4, 'Hyper', 'hhypernanoo@gmail.com', 62),
+(5, 'Alperen Gedik', 'aalperengedikk@gmail.com', 71),
+(6, 'Hyper Nano', 'hhypernanoo@gmail.com', 48);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
